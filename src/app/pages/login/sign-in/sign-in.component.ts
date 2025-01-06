288e0f1b3d51deab6ec1d59f16be8d2dbf49030c
kaizen <belebraz16@gmail.com>
2025-01-05 21:43:54 -0300

    wip: login page sign-in

---
+++ src/app/pages/login/sign-in/sign-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [MatIconModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss'
+})
+export class SignInComponent {
+
+}
